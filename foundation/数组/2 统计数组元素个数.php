3047314f14adee01adf234b3b4cf4c3b16342888
Zhichao Han <zhchhan@gmail.com>
2018-01-23 22:22:36 +1100

    commit array

---
+++ foundation/数组/2 统计数组元素个数.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>统计数组元素个数</title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<?php 
+		$arr1 = array("1","asid","19",true,"秋叶");
+		$co = count($arr1);
+		echo "数组中存在元素".$co."个";
+	?>
+</body>
+</html>
